76e42a7eba68ea8b7f8e2f0448651539164bcfec
Johan Lindskogen <johan.lindskogen@gmail.com>
2019-12-02 21:36:44 +0100

    Reindent

--- src/day1/task1/__tests__/task1.spec.ts
+++ src/day1/task1/__tests__/task1.spec.ts
@@ -1,12 +1,14 @@
 import { getFuel } from "../task1";
 
-
 test.each`
   value     | expected
   ${12}     | ${2}
   ${14}     | ${2}
   ${1969}   | ${654}
   ${100756} | ${33583}
-`("getFuel returns $expected for example input: $value", ({ value, expected }) => {
-  expect(getFuel(value)).toBe(expected);
-});
+`(
+  "getFuel returns $expected for example input: $value",
+  ({ value, expected }) => {
+    expect(getFuel(value)).toBe(expected);
+  }
+);
