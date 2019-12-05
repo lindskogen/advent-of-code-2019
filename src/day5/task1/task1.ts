7cac481cfecfa68cf4e32fab0a764f6c553db948
Johan Lindskogen <johan.lindskogen@gmail.com>
2019-12-05 08:37:55 +0100

    Day 5 task 1

---
+++ src/day5/task1/task1.ts
@@ -0,0 +1,101 @@
+export const parseInput = (input: string): number[] =>
+  input.split(",").map(str => parseInt(str, 10));
+
+enum ParameterMode {
+  Position = 0,
+  Immeditate = 1
+}
+
+export const calculateParameterModes = (
+  input: number
+): [[ParameterMode, ParameterMode, ParameterMode], number] => {
+  let parameterMode: [ParameterMode, ParameterMode, ParameterMode] = [0, 0, 0];
+  let instruction = input;
+  if (instruction > 99) {
+    let x = Math.floor(instruction / 100);
+
+    parameterMode = [
+      x % 10,
+      Math.floor((x / 10) % 10),
+      Math.floor((x / 100) % 10)
+    ];
+    instruction = Math.floor(instruction % 100);
+  }
+
+  return [parameterMode, instruction];
+};
+
+const getValueWithParameterMode = (
+  opCodes: number[],
+  param: number,
+  paramMode: ParameterMode
+): number => {
+  switch (paramMode) {
+    case ParameterMode.Position:
+      return opCodes[param];
+    case ParameterMode.Immeditate:
+      return param;
+    default:
+      throw new Error("Unknown parameter mode " + paramMode);
+  }
+};
+
+export const runProgram = (opCodes: number[], inputs: number[]): number[] => {
+  let outputs = [];
+  let inputIndex = 0;
+  const getInput = () => {
+    let number = inputs[inputIndex];
+    if (number === undefined) {
+      throw new Error("Cannot read input at index " + inputIndex);
+    }
+    inputIndex++;
+    return number;
+  };
+
+  let index = 0;
+  wh: while (true) {
+    const [[pmode1, pmode2], instruction] = calculateParameterModes(
+      opCodes[index]
+    );
+
+    switch (instruction) {
+      case 99:
+        break wh;
+      case 1: {
+        let p1 = opCodes[index + 1];
+        let p2 = opCodes[index + 2];
+        let p3 = opCodes[index + 3];
+        let v1 = getValueWithParameterMode(opCodes, p1, pmode1);
+        let v2 = getValueWithParameterMode(opCodes, p2, pmode2);
+
+        opCodes[p3] = v1 + v2;
+        index += 4;
+        break;
+      }
+      case 2: {
+        let p1 = opCodes[index + 1];
+        let p2 = opCodes[index + 2];
+        let p3 = opCodes[index + 3];
+        let v1 = getValueWithParameterMode(opCodes, p1, pmode1);
+        let v2 = getValueWithParameterMode(opCodes, p2, pmode2);
+
+        opCodes[p3] = v1 * v2;
+        index += 4;
+        break;
+      }
+      case 3: {
+        let p1 = opCodes[index + 1];
+        opCodes[p1] = getInput();
+        index += 2;
+        break;
+      }
+      case 4: {
+        let p1 = opCodes[index + 1];
+        outputs.push(getValueWithParameterMode(opCodes, p1, pmode1));
+        index += 2;
+        break;
+      }
+    }
+  }
+  return outputs;
+};
