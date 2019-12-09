2fab7f4bc3ae349575b4eff2668c607c94d34c91
Johan Lindskogen <johan.lindskogen@gmail.com>
2019-12-09 15:30:58 +0100

    Day 9 task 2

---
+++ src/day9/task2/main.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+import * as path from "path";
+import { parseInput } from "../../intcode/parse";
+import { runProgramWithSyncInput } from "../../intcode/program";
+
+fs.readFile(
+  path.join(__dirname, "input"),
+  "utf8",
+  async (error: Error | null, data: string) => {
+    const program = await runProgramWithSyncInput(parseInput(data), [2]);
+    console.log(program);
+  }
+);
